f28a7463d239f2652725c5e025dfa7580d454d38
rajat kumar <sevenstargarg6@gmail.com>
2018-09-14 01:19:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rajatkumar/homenetwork/StartActivity.java
@@ -0,0 +1,23 @@
+package com.example.rajatkumar.homenetwork;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class StartActivity extends Activity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.welcomeText);
+    }
+
+    public void welcomeTextClicked(View view) {
+        Intent intent = new Intent(StartActivity.this, MainActivity.class);
+    }
+}
