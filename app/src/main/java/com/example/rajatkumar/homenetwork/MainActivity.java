464d239ed024a0c2ac959fbfa1954d189b01331e
rajat kumar <sevenstargarg6@gmail.com>
2018-09-14 01:54:07 -0400

    Commit 1

--- app/src/main/java/com/example/rajatkumar/homenetwork/MainActivity.java
+++ app/src/main/java/com/example/rajatkumar/homenetwork/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.rajatkumar.homenetwork;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem mi) {
+        switch (mi.getItemId()) {
+            case R.id.action_user:
+
+                Snackbar.make(findViewById(R.id.coordinatorLayout),"Team 4", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Log.d("Toolbar", "Option 1 selected");
+                break;
+
+            case R.id.action_about:
+                Toast toast = Toast.makeText(this, "Version 1.0 by Team 4", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+        }
+
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/rajatkumar/homenetwork/StartActivity.java
+++ app/src/main/java/com/example/rajatkumar/homenetwork/StartActivity.java
@@ -12,12 +12,13 @@ public class StartActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
 
         textView = (TextView)findViewById(R.id.welcomeText);
     }
 
     public void welcomeTextClicked(View view) {
         Intent intent = new Intent(StartActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
